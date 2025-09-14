4eb3a31d0e25aac70f4e2238a89fcb6341f89079
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-14 15:02:11 -0500

    organized the code

---
+++ ApiDataAccess/Client.cs
@@ -0,0 +1,13 @@
+public class Client
+{
+
+  public async Task<string> Read(string baseAddress, string requestUri)
+  {
+    var client = new HttpClient();
+    client.BaseAddress = new Uri(baseAddress);
+    var response = await client.GetAsync(requestUri);
+    response.EnsureSuccessStatusCode();
+
+    return await response.Content.ReadAsStringAsync();
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.Json;
-using System.Text.Json.Serialization;
 
 var taco = new Client();
 var baseAddress = "https://swapi.info/api/";
@@ -14,34 +13,3 @@ foreach (var planet in data)
 }
 
 Console.ReadKey();
-
-public class Client
-{
-
-  public async Task<string> Read(string baseAddress, string requestUri)
-  {
-    var client = new HttpClient();
-    client.BaseAddress = new Uri(baseAddress);
-    var response = await client.GetAsync(requestUri);
-    response.EnsureSuccessStatusCode();
-
-    return await response.Content.ReadAsStringAsync();
-  }
-}
-
-    public record Root(
-        [property: JsonPropertyName("name")] string name,
-        [property: JsonPropertyName("rotation_period")] string rotation_period,
-        [property: JsonPropertyName("orbital_period")] string orbital_period,
-        [property: JsonPropertyName("diameter")] string diameter,
-        [property: JsonPropertyName("climate")] string climate,
-        [property: JsonPropertyName("gravity")] string gravity,
-        [property: JsonPropertyName("terrain")] string terrain,
-        [property: JsonPropertyName("surface_water")] string surface_water,
-        [property: JsonPropertyName("population")] string population,
-        [property: JsonPropertyName("residents")] IReadOnlyList<string> residents,
-        [property: JsonPropertyName("films")] IReadOnlyList<string> films,
-        [property: JsonPropertyName("created")] DateTime created,
-        [property: JsonPropertyName("edited")] DateTime edited,
-        [property: JsonPropertyName("url")] string url
-    );
