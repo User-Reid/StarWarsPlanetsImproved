57241eb141446fe78bae35876cee5285815caa6c
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-26 19:12:15 -0500

    finished teh apireader file.

--- ApiDataAccess/ApiReader.cs
+++ ApiDataAccess/ApiReader.cs
@@ -2,4 +2,4 @@ public interface IApiDataReader
 {
   public Task<string> Read(string baseAddress, string requestUri);
 }
-System.Console.WriteLine("");
+System.Console.WriteLine("Im so sorry, i will continue next week.");
